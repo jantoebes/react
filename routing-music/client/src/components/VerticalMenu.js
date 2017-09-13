270909f81089d2776597e0dfd6a10c3a794043df
Jan Toebes <jan@toebes.info>
2017-09-13 22:16:23 +0200

    -

---
+++ routing-music/client/src/components/VerticalMenu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+import '../styles/VerticalMenu.css';
+
+const VerticalMenu = ({ albums, albumsPathname }) => (
+  <div className='ui secondary vertical menu'>
+    <div className='header item'>
+      Albums
+    </div>
+    {
+      albums.map((album) => {
+        const to = `${albumsPathname}/${album.id}`;
+        return <NavLink
+          to={to}
+          activeClassName='active'
+          className='item'
+          key={album.id}
+        >
+          {album.name}
+        </NavLink>
+        })
+    }
+  </div>
+);
+
+export default VerticalMenu;
